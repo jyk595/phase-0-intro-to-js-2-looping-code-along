44b99011ad46b4efd6b60fde20670ddd9e894fea
John <jyk595@gmail.com>
2021-05-28 22:50:47 -0700

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const name = ["Ada", "Brendan", "Ali"];
+
+function writeCards(name, eventName) {
+    let newArray = [];
+    
+    for(let i=0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return newArray;
+};
+
+// function countDown(number) {
+//     let i = number-1;
+//     while (i >= 0) {
+//         console.log(number);
+//         i --;
+//     }
+// }
+
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown --);
+    }
+    return countDown;
+}
